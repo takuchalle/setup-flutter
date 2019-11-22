9c7644624038caaef016e188fadb5ae68d4e3cf5
Takuya OHASHI <tky.ohashi@gmail.com>
2019-11-23 08:57:06 +0900

    Download and extract

--- src/installer.ts
+++ src/installer.ts
@@ -1,29 +1,72 @@
+// Load tempDirectory before it gets wiped by tool-cache
+let tempDirectory = process.env['RUNNER_TEMPDIRECTORY'] || '';
 import * as core from '@actions/core';
+import * as tc from '@actions/tool-cache';
 import * as os from 'os';
 import * as path from 'path';
 import * as util from 'util';
 
-
 const baseUrl = 'https://storage.googleapis.com/flutter_infra/releases';
 
-export function getFlutter(version: string, channel: string) {
-    let filename = getFileName(version, channel)
-    let url = getFileUrl(filename, channel)
+if (!tempDirectory) {
+    let baseLocation;
+    if (process.platform === 'win32') {
+      // On windows use the USERPROFILE env variable
+      baseLocation = process.env['USERPROFILE'] || 'C:\\';
+    } else {
+      if (process.platform === 'darwin') {
+        baseLocation = '/Users';
+      } else {
+        baseLocation = '/home';
+      }
+    }
+    tempDirectory = path.join(baseLocation, 'actions', 'temp');
+  }
 
-    console.log(url)
+export async function getFlutter(version: string, channel: string) {
+    await acquireFlutter(version, channel);
+}
+
+async function acquireFlutter(version: string, channel: string) : Promise<string> {
+    let filename = getFileName(version, channel)
+    let downloadUrl = getFileUrl(filename, channel)
+    let downloadPath: string | null = null;
+    try {
+      downloadPath = await tc.downloadTool(downloadUrl);
+    } catch (error) {
+      core.debug(error);
+  
+      throw `Failed to download ${filename}: ${error}`;
+    }
+
+  //
+  // Extract
+  //
+  let extPath: string = tempDirectory;
+  if (!extPath) {
+    throw new Error('Temp directory not set');
+  }
+
+  if (process.platform == 'linux') {
+    extPath = await tc.extractTar(downloadPath);
+  } else {
+    extPath = await tc.extractZip(downloadPath);
+  }
+  console.log(extPath)
+  return extPath
 }
 
 function getPlatformName() : string {
     switch(os.platform()) {
         case 'darwin': { return 'macos' }
         case 'win32': { return 'windows'}
-        default: { return 'linunx' }
+        default: { return 'linux' }
     }
 }
 
 function getFileName(version: string, channel: string): string {
     const platform: string = getPlatformName();
-    const ext: string = platform == 'linunx' ? 'tar.xz' : 'zip';
+    const ext: string = platform == 'linux' ? 'tar.xz' : 'zip';
     const filename: string = util.format(
       'flutter_%s_%s-%s.%s',
       platform,
